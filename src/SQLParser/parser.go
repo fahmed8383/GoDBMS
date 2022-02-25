b65ec2b59dae2ff67cd8b47d673c42ece5a7472f
qureshe <qureshe@mcmaster.ca>
2022-02-25 14:36:49 -0500

    Added CLI

--- src/SQLParser/parser.go
+++ src/SQLParser/parser.go
@@ -1,7 +1,59 @@
 package SQLParser
 
-import "fmt"
+import ("fmt"
+        "strings"
+        "bufio"
+        "os")
 
-func Print() {  
-    fmt.Println("I am the parser to parse the SQL queries")
+func ParseInput() {  
+    fmt.Println("Welcome to GoDBMS")
+
+
+    for true{
+
+    //iniate buffer to read standard input delimited by newline
+	reader := bufio.NewReader(os.Stdin)
+	input, err1 := reader.ReadString('\n')
+
+	if err1 != nil {
+		fmt.Println("An error occured while reading input. Please try again", err1)
+		return
+	}
+
+    //remove delimiter from string, convert chars to lowercase, split string by word
+    input = strings.TrimSuffix(input, "\n")
+    query := strings.ToLower(input) 
+    querySplit := strings.Split(query, " ")
+
+    fmt.Println(query)
+
+
+    //if the user input is a 'create table' query call parseCreateTableQuery 
+    if querySplit[0] == "create" && querySplit[1] == "table"{
+            output, err := parseCreateTableQuery(query)
+            if err != nil {
+                fmt.Println(err)
+                
+            }else{
+            fmt.Println(*output)}
+        }
+
+     //if the user input is a 'insert tuple' query call parseInsertTupleQuery    
+    if querySplit[0] == "insert" && querySplit[1] == "into"{
+            output, err := parseInsertTupleQuery(query)
+            if err != nil {
+                fmt.Println(err)
+                
+            }else{
+            fmt.Println(*output)}
+        
+        }
+
+    //if user enters 'quit' command stop taking input
+    if querySplit[0] == "quit"{
+        break
+    }
+
+}
+    
 }
--- src/main.go
+++ src/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"GoDBMS/Database"
-	"GoDBMS/Modules"
 	"GoDBMS/SQLParser"
-	"fmt"
 )
 
 func main() {
-	fmt.Println("I am the main file")
-	SQLParser.Print()
-	Database.Print()
-	Modules.Print()
+	SQLParser.ParseInput()
 }
