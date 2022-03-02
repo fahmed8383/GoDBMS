c48dde379a264875f6c03540cce05cdbf4b279a7
kannammk <kannammk@mcmaster.ca>
2022-03-02 09:47:49 -0500

    added error checking

--- src/SQLParser/parse_insert_tuple.go
+++ src/SQLParser/parse_insert_tuple.go
@@ -9,12 +9,12 @@ import (
 type InsertTupleStatement struct {
 	// TableName is a string representing name of the table
 	TableName string
-	// Columns is an array of tupleColumn representing the columns
-	Columns []tupleColumn
+	// Columns is an array of insertTupleColumn representing the columns
+	Columns []insertTupleColumn
 }
 
-// tupleColumn holds the insert tuple query column info received from the user
-type tupleColumn struct {
+// insertTupleColumn holds the insert tuple query column info received from the user
+type insertTupleColumn struct {
 	// Name is a string representing name of the column
 	Name string
 	// Value is a string representing the value of the column
@@ -31,32 +31,54 @@ func parseInsertTupleQuery(query string) (*InsertTupleStatement, error) {
 	// Checks if the query has the correct number of arguments
 	if len(querySplit) > 3 {
 		return nil, errors.New("Insert tuple statement has too many arguments")
+	} else if len(querySplit) < 3 {
+		return nil, errors.New("Insert tuple statement has too few arguments")
 	}
 
 	// Extracts the table name and coverts it to lowercase
-	name := strings.Split(querySplit[0], " ")[2]
+	nameSplit := strings.Split(querySplit[0], " ")
+	// Checks if the query has the correct format for the table name
+	if len(nameSplit) != 4 {
+		return nil, errors.New("Insert tuple statement has invalid table name format")
+	}
+	name := nameSplit[2]
 
-	// Extracts the column names and trims them
-	columnSplit := strings.Split(querySplit[1], " ")
+	// Extracts the column section and trims them
+	columnSplit := strings.Split(querySplit[1], ")")
+
+	// Checks if the query has the correct syntax for insert statement
+	if len(columnSplit) < 2 {
+		return nil, errors.New("Insert tuple statement has invalid syntax")
+	}
+
+	if strings.Trim(columnSplit[1], " ") != "values" {
+		return nil, errors.New("Insert tuple statement is missing values keyword")
+	}
+
 	// Removes irrelevant words from the column names
 	// e.g. "[column1, column2),  VALUES]" -> "[column1, column2)]"
-	getColumns := columnSplit[0:len(querySplit)]
-	for i, v := range getColumns {
-		getColumns[i] = strings.Trim(v, " ,)")
-	}
+	columnTrim := columnSplit[0]
+	// Gets the column names by splitting with commas
+	getColumns := strings.Split(columnTrim, ",")
 
 	// Extracts the values for each corresponding column name and trims it
-	getValues := strings.Split(querySplit[2], " ")
+	getValues := strings.Split(querySplit[2], ",")
 	for i, v := range getValues {
-		getValues[i] = strings.Trim(v, " ,);")
+		getValues[i] = strings.Trim(v, " );")
 	}
 
-	// Initializes the array of tupleColumns
-	columns := []tupleColumn{}
-	// Goes through each column name and value, creates a tupleColumn struct
-	// and then adds them into the array of tupleColumns
+	// Initializes the array of insertTupleColumns
+	columns := []insertTupleColumn{}
+
+	// Checks if the number of columns and values are the same
+	if len(getColumns) != len(getValues) {
+		return nil, errors.New("Insert tuple statement has different number of columns and values")
+	}
+
+	// Goes through each column name and value, creates a insertTupleColumn struct
+	// and then adds them into the array of insertTupleColumns
 	for i := range getValues {
-		columnStruct := tupleColumn{getColumns[i], getValues[i]}
+		columnStruct := insertTupleColumn{getColumns[i], getValues[i]}
 		columns = append(columns, columnStruct)
 	}
 
