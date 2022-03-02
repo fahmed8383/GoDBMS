6ca338bc3b0fb21b0485a90f5f0d11486caa8c37
qureshe00 <qureshe@mcmaster.ca>
2022-03-02 17:21:31 -0500

    edited heap encoder

--- src/Controller/heap_encoder.go
+++ src/Controller/heap_encoder.go
@@ -9,7 +9,7 @@ import (
 
 // EncodeHeap is a function to serialize the catalog data structure into
 // bytes that can be written to a data file.
-func EncodeHeap() {
+func EncodeHeap(string name) (error) {
 
 	// Get a pointer to the current heap in memory.
 	heapPointer := Database.GetHeap()
@@ -21,24 +21,24 @@ func EncodeHeap() {
 
 	// Write the bytes from the buffer into a byte data file and log any errors
 	// that occur during the write.
-	err := WriteByteFile("heap", buffer.Bytes())
+	err := WriteByteFile(name, buffer.Bytes())
 	if err != nil {
-		log.Println(err)
+		return err
 	}
 }
 
 // DecodeHeap is a function to deserialize the heap data structure from
 // a byte data file on the disk.
-func DecodeHeap() {
+func DecodeHeap(string name) (error) {
 
 	// Check if the heap data file exists.
-	if FileExists("heap") {
+	if FileExists(name) {
 
 		// If it does exist, read the bytes from the file and log any errors
 		// that occur during the read.
-		heapBytes, err := ReadByteFile("heap")
+		heapBytes, err := ReadByteFile(name)
 		if err != nil {
-			log.Println(err)
+			return err
 		}
 
 		// Write the bytes into a byte buffer.
@@ -58,6 +58,6 @@ func DecodeHeap() {
 		
 		// If the heap does not already exist, initliaze an empty heap
 		// in memory.
-		Database.InitializeTables()
+		Database.InitializeHeap()
 	}
 }
