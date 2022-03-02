998833ed4fd080988a9738d9e83b73dc3b1f9e1b
qureshe00 <qureshe@mcmaster.ca>
2022-03-02 17:12:52 -0500

    Added heap decoder

--- src/Controller/heap_encoder.go
+++ src/Controller/heap_encoder.go
@@ -26,3 +26,38 @@ func EncodeHeap() {
 		log.Println(err)
 	}
 }
+
+// DecodeHeap is a function to deserialize the heap data structure from
+// a byte data file on the disk.
+func DecodeHeap() {
+
+	// Check if the heap data file exists.
+	if FileExists("heap") {
+
+		// If it does exist, read the bytes from the file and log any errors
+		// that occur during the read.
+		heapBytes, err := ReadByteFile("heap")
+		if err != nil {
+			log.Println(err)
+		}
+
+		// Write the bytes into a byte buffer.
+		buffer := bytes.NewBuffer(heapBytes)
+		
+		// Initialize the heap datastructure.
+		var heap map[string]*Database.TableSchema
+
+		// Decode the byte buffer into the heap data structure.
+		dec := gob.NewDecoder(buffer)
+		dec.Decode(&heap)
+
+		// Load the heap into memory.
+		Database.LoadTablesMap(&heap)
+		
+	} else {
+		
+		// If the heap does not already exist, initliaze an empty heap
+		// in memory.
+		Database.InitializeTables()
+	}
+}
