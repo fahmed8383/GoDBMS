22186d8f2152f8b2b22311da7c9959d5c1267dfa
kannammk <kannammk@mcmaster.ca>
2022-03-03 03:12:51 -0500

    commented process insert tuple

--- src/Controller/process_insert_tuple.go
+++ src/Controller/process_insert_tuple.go
@@ -3,58 +3,70 @@ package Controller
 import (
 	"GoDBMS/Database"
 	"errors"
-	"fmt"
 )
 
+// ProcessInsertTuple is a function that handles the insertion of a tuple into
+// the database. It takes an InsertTupleStatement pointer as an argument and
+// returns any errors that occur
 func ProcessInsertTuple(insertTuple *Database.InsertTupleStatement) (err error) {
+
+	// Declares the table that will be used later
 	var table *Database.TableSchema
 
-	// check if table exists, if not, return error
+	// If the table does not exist, return an error stating it
+	// Otherwise, it stores the table in the variable
 	if Database.TableExists(insertTuple.TableName) {
 		table = Database.GetTable(insertTuple.TableName)
 	} else {
-		err = fmt.Errorf("Table %s does not exist", insertTuple.TableName)
+		err = errors.New("Table " + insertTuple.TableName + " does not exist")
 		return err
 	}
 
-	// load heap, decode heap
+	// Deserializes the heap from the file and loads it
+	// An error is returned if one occurs
 	err = DecodeHeap(table.Name)
 	if err != nil {
 		return err
 	}
 
-	heap := Database.GetHeap()
-
-	// Prints tuples in heap
-	for _, tuple := range *heap {
-		fmt.Println("Tuple", tuple)
-	}
-
+	// The primary key of the tuple
 	tupleKey := insertTuple.Columns[table.PrimaryKeyIndex].Value
 
-	// check if tuple with primary key already exists
+	// Utilizes the TupleExists function to check if the tuple already exists
+	// If so, it returns an error stating so
 	if Database.TupleExists(tupleKey, table.PrimaryKeyIndex) {
-		err = fmt.Errorf("Tuple with key %s already exists", tupleKey)
+		err = errors.New("Tuple with key " + tupleKey + " already exists")
 		return err
 	}
 
+	// Creates a new array of type InsertTupleColumn to store the values of the tuple
+	// It uses make to create an array with dynamic size using the length of columns
+	// in the table schema
 	finalColumns := make([]Database.InsertTupleColumn, len(table.Columns))
 
+	// Iterates through the columns of the InsertTupleStatement query and adds
+	// them to the finalColumns array in their corresponding index
+	// The index is determined by the hash map ColumnIndex in the table schema
 	for _, col := range insertTuple.Columns {
+		// If the column does not exist in the table schema, exists would be false
+		// Which would then be used to return an error
 		index, exists := table.ColumnIndex[col.Name]
 		if exists != true {
 			err = errors.New("Column " + col.Name + " does not exist")
 			return err
 		}
+		// Stores a new InsertTupleColumn in the finalColumns array at the index
 		finalColumns[index] = Database.InsertTupleColumn{col.Name, col.Value}
 	}
 
+	// Iterates through finalColumns and checks for missing columns/null indices
+	// If the column is empty and it can't be Null, it returns an error
 	for i, col := range finalColumns {
 		if col.Name == "" {
 			if table.Columns[i].NotNull {
-				// fmt.Println(table.Columns[i])
 				err = errors.New("Column " + table.Columns[i].Name + " canot be null")
 				return err
+				// If the column can be null, it assigns the name attribute
 			} else {
 				finalColumns[i].Name = table.Columns[i].Name
 			}
@@ -62,16 +74,16 @@ func ProcessInsertTuple(insertTuple *Database.InsertTupleStatement) (err error) 
 		}
 	}
 
+	// Replaces the values of the insertTuple with the finalColumns array
 	insertTuple.Columns = finalColumns
 
-	// create tuple
-	// insert tuple into table using heap
-
+	// Creates a new Tuple pointer using the CreateTuple function
 	tuple, err := Database.CreateTuple(table, insertTuple)
 
+	// Inserts the tuple into the heap
 	Database.InsertTuple(tuple)
 
-	// save heap, encode heap
+	// Encodes the heap and saves it to the file
 	err = EncodeHeap(table.Name)
 	if err != nil {
 		return err
